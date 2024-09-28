0b39566f896deed00a5c152e900a5f44c5204eb7
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2024-09-28 15:42:50 +0530

    C++ update

--- C++/C++ Basics Question Practice/Arrays and Strings/12_length_of_string.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-
-int main() {
-    string s = "Hello World";
-    cout << s.size() << endl;
-    return 0;
-}
--- C++/C++ Basics Question Practice/Basic Syntax and Control Structures/2_input_and_print.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-
-int main() {
-    int a;
-    std::cin>> a;
-    std::cout<<"Value of a is " << a<<std::endl;
-    return 0;
-}
