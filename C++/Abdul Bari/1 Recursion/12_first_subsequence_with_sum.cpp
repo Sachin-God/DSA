01c7a43a846ed617b5afcb428b41a131e9e1589f
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2024-10-03 15:17:06 +0530

    subsequence

---
+++ C++/Abdul Bari/1 Recursion/12_first_subsequence_with_sum.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void print(vector<int> arr) {
+    for (int i : arr){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
+void subsequence(int arr[], int idx, int size, int target){
+    static vector<int> vec;
+    static int total = 0;
+    static bool satisfied = false;
+
+    if (satisfied) {
+        return;
+    }
+    if (idx >= size) {
+        if (total == target){
+            satisfied = true;
+            print(vec);
+        }
+        return;
+    }
+    vec.push_back(arr[idx]);
+    total += arr[idx];
+    subsequence(arr, idx+1, size, target);
+    vec.pop_back();
+    total -= arr[idx];
+    subsequence(arr, idx+1, size, target);
+}
+
+int main() {
+    int arr[3] = {3,1,2};
+    subsequence(arr, 0, 3,3);
+    return 0;
+}
