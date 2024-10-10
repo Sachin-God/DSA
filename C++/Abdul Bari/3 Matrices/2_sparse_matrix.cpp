7bdb97b2f1d3354d5d3be97375b28fee94cc87fc
Sachin-God <113123578+Sachin-God@users.noreply.github.com>
2024-10-10 20:40:00 +0530

    Matrix

---
+++ C++/Abdul Bari/3 Matrices/2_sparse_matrix.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+struct Sparse {
+    int m, n;
+    int num;
+    struct Element *e;
+};
+
+struct Element {
+    int i, j, x;
+};
+
+void create(struct Sparse* A){
+    
+}
+int main() {
+
+    return 0;
+}
